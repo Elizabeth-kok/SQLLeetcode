47471862393c57e2e9f994a03df13a3506a5f1b2
Elizabeth Kok <elizabethkok_97@hotmail.com>
2022-10-06 14:37:52 +0900

    sixth commit

--- Easy/Number of comments per post.sql
+++ Easy/Number of comments per post.sql
@@ -58,3 +58,9 @@
 -- The comment with id 6 is a comment on a deleted post with id 7 so we ignored it.
 
 -- Solution
+
+select s1.sub_id as post_id, ifnull(count(distinct(s2.sub_id)),0) as number_of_comments
+from submissions s1 left join submissions s2
+on s1.sub_id = s2.parent_id
+where s1.parent_id is Null
+group by s1.sub_id
